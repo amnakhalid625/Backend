fc98c29d2f364dac0541a1e64b409612c904d20c
amnakhalid625 <amnakhalidgsr@gmail.com>
2024-11-05 17:30:08 -0800

    classNo1

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('my twitter account')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>my login account</h1>')
+})
+
+app.get('/kid',(req,res)=>{
+    res.send('<input type="number" placeholder="enter you joke"/>')
+})
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
